72cc7c46591feaef1a60abcbe306b39c4b868c28
周少行~ <1181842363@qq.com~>
2023-04-21 13:43:53 +0800

    123

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import AboutView from './AboutView.vue';
+console.log(AboutView);
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
